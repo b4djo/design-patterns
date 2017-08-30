0aa0ab8c118bd47303d9ff8245f1c7e079ee0c80
Robert <fatkhullin.robert@gmail.com>
2017-08-30 18:21:30 +0300

    <bridge> PSR2

--- Structural/Bridge/PlainTextFormatter.php
+++ Structural/Bridge/PlainTextFormatter.php
@@ -6,7 +6,8 @@ namespace DesignPatterns\Structural\Bridge;
  * Class PlainTextFormatter
  * @package DesignPatterns\Structural\Bridge
  */
-class PlainTextFormatter implements FormatterInterface {
+class PlainTextFormatter implements FormatterInterface
+{
     
     public function format(string $text)
     {
