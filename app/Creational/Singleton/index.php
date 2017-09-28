af27e940dfe233f5e9cc132c6be3e8af4223b24d
Robert <fatkhullin.robert@gmail.com>
2017-09-28 13:05:14 +0300

    <singleton> Fixes, added test

--- app/Creational/Singleton/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Autoloader classes
-require __DIR__ . '/../../app/AutoLoader.php';
-$autoloader = new \app\lib\AutoLoader();
-$autoloader->addNamespace('DesignPatterns\\Creational\\Singleton', realpath(__DIR__));
-$autoloader->register();
-
-$singleton = \DesignPatterns\Creational\Singleton\Singleton::getInstance();
